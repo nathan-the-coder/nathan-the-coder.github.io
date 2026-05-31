6b2b761405bc5459b4cd2789e239df713322d707
Nathaniel Ramos <nathanielramos726@gmail.com>
2026-05-31 19:09:55 +0800

    Initial commit

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"]
+});
+
+const ovo = Ovo({
+  subsets: ["latin"],
+  weight: ["400"]
+});
+
+export const metadata: Metadata = {
+  title: "Portfolio - Nathaniel Ramos",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${outfit.className} ${ovo.className} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
